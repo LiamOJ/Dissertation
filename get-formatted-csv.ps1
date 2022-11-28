4ff24ed1fda78637a95fdba9a79b7b961e5faa8b
LiamOJ <48185797+LiamOJ@users.noreply.github.com>
2022-11-28 20:26:14 +0000

    Delete get-formatted-csv.ps1

--- get-formatted-csv.ps1
+++
@@ -1,7 +0,0 @@
-﻿$csv = import-csv .\filtered_evid10.csv | 
-    select grantedaccess,
-    @{Name="decimal_grantedaccess";Expression={[Int]$_.decimal_grantedaccess}},
-    @{Name="unknown_count";Expression={[Int]$_.unknown_count}},
-    @{name='calltrace_count';expression={[Int]$_.calltrace_count}},
-    @{name='unique_call_steps';expression={[Int]$_.unique_call_steps}},
-    injected
